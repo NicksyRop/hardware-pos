c5603b4dcc431288881899951cd9d89086fa73c5
NicksyRop <nicksonkipkorir25@gmail.com>
2021-08-20 18:47:20 +0300

    changed database to postgress and forced http suring production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') == 'production'){
+
+            \URL::forceScheme('https');
+        }
     }
 }
+postgres://qvstlvttlsjkzu:a742b6896c749fbaa6ba937d71440d911999d9fe0b0dbcbf1e3a6bc236c96892@ec2-18-214-238-28.compute-1.amazonaws.com:5432/d3pr0s0a57ijvi
